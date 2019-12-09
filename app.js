d256ae6c0ef1865a975f4e2d3eaf53fb0eb4e33b
Julian Johannesen <julian.johannesen@gmail.com>
2019-12-09 12:50:14 -0500

    working on pug templates

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const data = require('./data.json');
+// Used when setting express.static()
 const path = require('path');
 
 // Set view engine
 app.set('view engine', 'pug')
-// 
+// Use express.static to set the root folder and any options
+app.use(express.static('public'))
+
+/*
+ROUTES
+*/
+
+// Render a home page
 app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+  })  
+
+
+  app.get('/', function (req, res) {
     res.render('index', { title: 'Hey', message: 'Hello there!' })
   })  
